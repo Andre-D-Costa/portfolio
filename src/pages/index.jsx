346a5f45df8f150734c83394a55b809a70f5513c
André Costa <costadavidandre@gmail.com>
2025-03-06 21:00:16 +0000

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <>
+      <nav>Menu</nav>
+      <main>Content</main>
+      <footer>Footer</footer>
+    </>
+  );
+}
