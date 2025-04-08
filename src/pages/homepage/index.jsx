a97ace7e8a873906973257d7733fc5e76231c4d2
André Costa <costadavidandre@gmail.com>
2025-04-08 17:03:06 +0100

    changed cover image for mobile.

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 import "./homepage.css";
-// import head3D from "../../assets/3d_thumb.png";
-// import capitan from "../../assets/07.jpg";
-import baloon from "../../assets/010.jpg";
 import cover from "../../assets/des3.jpg";
-// import werew from "../../assets/17.png";
-// import shadow from "../../assets/7686787.png";
-// import faceDraw from "../../assets/face53453454.png";
-// import framesPerf from "../../assets/justlivin4.png";
+import framesPerf from "../../assets/justlivin4.png";
 
 export default function HomePage() {
   return (
@@ -16,26 +10,17 @@ export default function HomePage() {
         <p className="text__hey">Hey</p>
         <p className="text__comma">,</p>
         <p className="text__welcome">welcome</p>
-        {/* <img src={head3D} alt="" /> */}
+        <p className="text__welcome2">
+          wel-
+          <br />
+          come
+        </p>
+        <img className="homepage__image--cover" src={cover} alt="cover_pic" />
         <img
-          className="homepage__images--capitan"
-          src={cover}
-          alt="El Cápitan"
+          className="homepage__image--coverMobile"
+          src={framesPerf}
+          alt="cover__pic--mobile"
         />
-        {/* <img
-          className="homepage__images--capitan"
-          src={capitan}
-          alt="El Cápitan"
-        /> */}
-        <img
-          className="homepage__images--baloon"
-          src={baloon}
-          alt="The Baloon"
-        />
-        {/* <img src={werew} alt="" /> */}
-        {/* <img src={shadow} alt="" /> */}
-        {/* <img src={faceDraw} alt="" /> */}
-        {/* <img src={framesPerf} alt="" /> */}
       </div>
     </main>
   );
